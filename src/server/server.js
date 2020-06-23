ec14b63e35abb91d3d1612877c995c9b772e153f
htmlcssphpjs <63463168+htmlcssphpjs@users.noreply.github.com>
2020-06-23 17:41:02 +0300

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -11,20 +11,6 @@ const webpackConfig = require('../../webpack.dev.js');
 const app = express();
 app.use(express.static('public'));
 
-if (process.env.NODE_ENV === 'development') {
-  // Setup Webpack for development
-  const compiler = webpack(webpackConfig);
-  app.use(webpackDevMiddleware(compiler));
-} else {
-  // Static serve the dist/ folder in production
-  app.use(express.static('dist'));
-}
-
-// Listen on port
-const port = process.env.PORT || 3000;
-const server = app.listen(port);
-console.log(`Server listening on port ${port}`);
-
 // Setup socket.io
 const io = socketio(server);
 
